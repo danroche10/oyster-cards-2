9081f8f85d082b8568b875d1112efd9d79969486
danroche10 <danielroche10@gmail.com>
2021-08-06 18:00:09 +0100

    need to mover journey_history to journey_log

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -24,26 +24,30 @@ class Oystercard
   def touch_in(station)
     raise "Insufficient funds" if @balance < MIN_CHARGE
     
-    no_tap_out if @current_journey.forgot_to_tap_out?
+    forgot_tap_out if @current_journey.forgot_to_tap_out?
     @current_journey.start_journey(station)
   end
 
   def touch_out(station)
     @current_journey.finish_journey(station)
-    deduct(1)
+    deduct(fare)
     @journey_history << @current_journey.status
     @current_journey = Journey.new
   end
 
+  def check_journey
+    @current_journey.in_journey?
+  end
+
   private
 
-  # def fare
-  #   if # journey not complete
-
-  #     PENALTY_FARE
-  #   else MIN_CHARGE
-  #   end
-  # end
+  def fare
+    if current_journey.status[:exit_station] == "None recorded" && current_journey.status[:entry_station] != "None recorded"
+      PENALTY_FARE
+    else
+      MIN_CHARGE
+    end
+  end
 
   def forgot_tap_out
     deduct(PENALTY_FARE)
@@ -57,13 +61,36 @@ class Oystercard
 
 end
 
-# log = JourneyLog.new
+# Full journey
+
+
+log = Journey
 my_oyster = Oystercard.new
 my_oyster.top_up(50)
 my_oyster.touch_in("Southgate")
-p my_oyster.current_journey
 my_oyster.touch_out("Farringdon")
 p my_oyster.balance
-p my_oyster.current_journey
-p my_oyster.journey_history 
-# p log.journey_history 
+p my_oyster.journey_history
+
+# forgot tap in
+
+my_oyster.touch_out("Farringdon")
+p my_oyster.balance
+p my_oyster.journey_history
+
+# forgot to tap out
+my_oyster.touch_in("Southgate")
+
+# and then a full journey again
+my_oyster.touch_in("Southgate")
+my_oyster.touch_out("Morden")
+p my_oyster.balance
+p my_oyster.journey_history
+
+
+
+
+
+
+
+
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -25,7 +25,7 @@ describe Oystercard do
     it 'returns and error if the new balance exceeds max amount' do
       expect { 10.times { subject.top_up(10) } }.to raise_error "Max amount exceeded"
     end
-
+  
   end
 
   describe '#deduct' do
@@ -34,36 +34,6 @@ describe Oystercard do
     #   expect(subject).to respond_to(:deduct).with(1).argument
     #  end
      
-    it 'tops up an oyster card' do
-      result = card.send(:deduct,5)
-      expect(result).to eq 5
-     end
-  end
-
-  describe '#in_journey?' do
-    
-    it 'a new card is out of use' do
-      expect(subject).not_to be_in_journey
-    end
-
-    context 'after touching in' do
-
-      it 'it is in use' do
-        card.touch_in(station)
-        expect(card).to be_in_journey
-      end
-    
-    end
-
-    context 'after touching out' do
-
-      it 'it out of use' do
-        card.touch_in(station)
-        card.touch_out(station)
-        expect(card).not_to be_in_journey
-      end
-
-    end
 
   end
 
@@ -71,11 +41,6 @@ describe Oystercard do
 
     it 'returns an error if balance is below £1' do
       expect { subject.touch_in(station) }.to raise_error "Insufficient funds"
-    end
-
-    it 'remembers touch  in station' do
-      card.touch_in(station)
-      expect(card.entry_station).to eq station
     end
 
   end
@@ -86,24 +51,7 @@ describe Oystercard do
       expect { card.touch_out(station) }.to change { card.balance }.by(-1)
     end
 
-    it 'forgets entry station' do
-      card.touch_in(station)
-      card.touch_out(station)
-      expect(card.entry_station).to be_nil
-    end
   end
-
-  describe 'journeys' do
-
-    it 'is empty by default' do
-      expect(subject.journeys).to be_empty
-    end
-
-    it 'creates one journey from touching in and out' do
-      card.touch_in(station)
-      expect { card.touch_out(station) }.to change { card.journeys.count }.by(1)
-    end
-  end
-  
+    
 end
 
